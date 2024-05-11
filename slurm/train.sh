f784d528f6e2c8411755c97337de22acd697960b
Evan Kim <evan_kim@OPENMIND7.MIT.EDU>
2024-05-11 03:08:23 -0400

    updated requirements and train script

--- slurm/train.sh
+++ slurm/train.sh
@@ -2,17 +2,16 @@
 
 set -x
 
-if [[ $# -lt 3 ]] ; then
+if [[ $# -lt 2 ]] ; then
     echo 'too few arguments supplied'
     exit 1
 fi
 
-PARTITION=$1
-NAME=$2
-OPTIONS=$3
+NAME=$1
+OPTIONS=$2
 
-srun -p ${PARTITION} \
-    --job-name=Mesh \
+srun \
+    --job-name=Tp2m \
     --gres=gpu:8 \
     --ntasks=1 \
     --kill-on-bad-exit=1 \
